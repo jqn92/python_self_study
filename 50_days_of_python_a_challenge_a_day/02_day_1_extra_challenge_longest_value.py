60b74d6b97489727315968e81a2947f479c4ed4f
jqn92 <jrodriguezalvarez.92@proton.me>
2025-02-05 18:24:07 -0300

    Upload exercise to solve later

---
+++ 50_days_of_python_a_challenge_a_day/02_day_1_extra_challenge_longest_value.py
@@ -0,0 +1,9 @@
+# Page 10
+
+# Write a function called longest_value that takes a dictionary
+# as an argument and returns the first longest value of the
+# dictionary. For example, the following dictionary should return
+# "apple" as the longest value.
+# fruits = {'fruit': 'apple', 'color': 'green'}
+
+def longest_value(dict):
