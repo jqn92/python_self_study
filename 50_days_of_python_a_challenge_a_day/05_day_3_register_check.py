98fe48a010293b39cd1746690ae5a88c1c8893d4
jqn92 <jrodriguezalvarez.92@proton.me>
2025-02-19 15:23:19 -0300

    Upload exercise to solve later

---
+++ 50_days_of_python_a_challenge_a_day/05_day_3_register_check.py
@@ -0,0 +1,12 @@
+# Page 12
+
+# Write a function called register_check that checks how many
+# students are in school. The function takes a dictionary as a
+# parameter. If the student is in school, the dictionary says ‘yes’. If
+# the student is not in school, the dictionary says ‘no’. Your
+# function should return the number of students in school. Use the
+# dictionary below. Your function should return 3.
+
+# register = {'Michael':'yes','John': 'no',
+# 'Peter':'yes', 'Mary': 'yes'}
+
