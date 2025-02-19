e79763967bdf7c5d4a555f7a9fedffb8944f447b
jqn92 <jrodriguezalvarez.92@proton.me>
2025-02-19 15:23:56 -0300

    Upload practice packages

---
+++ python_basics_a_practical_introduction_to_python_3/adder_main.py
@@ -0,0 +1,5 @@
+from adder import add, double
+
+value = add(2, 2)
+double_value = double(value)
+print(double_value)
---
+++ python_basics_a_practical_introduction_to_python_3/package_exercises/main.py
@@ -0,0 +1,8 @@
+from helpers.string import shout
+from helpers.math import area
+
+a = 5
+b = 8
+message = f"The area of a {a}-by-{b} rectangle is {area(a, b)}."
+
+print(shout(message))
